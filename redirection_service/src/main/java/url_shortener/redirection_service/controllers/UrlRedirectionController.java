4e65a953b635d51781f151662408da09ee1a0959
Belal Mohammed <belmoe94@gmail.com>
2025-05-26 19:45:19 +0400

    Add discovery_server module with Eureka server configuration. Update pom.xml for dependency management and properties. Include Maven wrapper files and initial application setup. Modify existing services to integrate with Eureka for service discovery.

--- redirection_service/src/main/java/url_shortener/redirection_service/controllers/UrlRedirectionController.java
+++ redirection_service/src/main/java/url_shortener/redirection_service/controllers/UrlRedirectionController.java
@@ -1,5 +1,6 @@
 package url_shortener.redirection_service.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import url_shortener.redirection_service.services.UrlMappingService;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/url")
 public class UrlRedirectionController {
@@ -17,13 +20,28 @@ public class UrlRedirectionController {
     @Autowired
     private UrlMappingService urlMappingService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("{alias}")
     public ResponseEntity<String> redirect(@PathVariable String alias) {
         String originalUrl = urlMappingService.findByUrlAlias(alias);
         if(originalUrl == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        log.info("Original Url " + originalUrl);
 
         return ResponseEntity.status(HttpStatus.FOUND).header("Location", originalUrl).build();
+
+//        try {
+//            String redirectionUrl = restTemplate.getForObject(originalUrl, String.class);
+//
+//            log.info("Redirection Url " + redirectionUrl);
+//
+//            return ResponseEntity.status(HttpStatus.FOUND).header("Location", redirectionUrl).build();
+//
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+//        }
     }
 }
--- shortener_service/src/main/java/url_shortener/shortener_service/ShortenerServiceApplication.java
+++ shortener_service/src/main/java/url_shortener/shortener_service/ShortenerServiceApplication.java
@@ -2,12 +2,13 @@ package url_shortener.shortener_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShortenerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShortenerServiceApplication.class, args);
 	}
-
 }
